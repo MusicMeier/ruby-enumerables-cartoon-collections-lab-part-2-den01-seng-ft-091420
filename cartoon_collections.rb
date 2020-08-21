bce666bc80bef2dcf74d33685d22dee04918937a
Music Meier <musicm987@gmail.com>
2020-08-21 22:34:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,7 +31,8 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.select do |string|
-   if string.find == valid_calls
+   if string == valid_calls
   end 
  end
+ valid_calls.find
 end
